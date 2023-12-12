e8172cda3abc1f10bfaaec5272f9ce800bff70bf
paul <paul7markets@hotmail.com>
2023-12-12 17:46:29 +0000

    new about me one

--- src/components/about/AboutIntro.tsx
+++ src/components/about/AboutIntro.tsx
@@ -12,18 +12,7 @@ const AboutIntro = () => {
 						As much as I love programming...
 					</p>
 					<p className="section-paragraph">
-						Some interesting topic about me that's not programming
-						related (all ideas welcome). Lorem Ipsum is simply dummy
-						text of the printing and typesetting industry. Lorem
-						Ipsum has been the industry's standard dummy text ever
-						since the 1500s, when an unknown printer took a galley
-						of type and scrambled it to make a type specimen book.
-					</p>
-					<p className="section-paragraph">
-						It was popularised in the 1960s with the release of
-						Letraset sheets containing Lorem Ipsum passages, and
-						more recently with desktop publishing software like
-						Aldus PageMaker including versions of Lorem Ipsum.
+						I also spend time away from the computer screen, often in the gym attempting to improve my fitness levels or finding solace in the simplicity of taking the dogs for a walk.
 					</p>
 				</div>
 			</section>
